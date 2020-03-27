de0763947dc3e026d84b727a89c266427c07a03c
Emerson Oliveira <emerson-ods@hotmail.com>
2020-03-26 22:25:29 -0300

    Initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+/* My Project Semana OmniStack */
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+const port = 3333;
+
+/**produção */
+// app.use(cors({
+//     origin: 'http://meuapp.com'
+// }));
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ frontend/src/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+//JSX (JavaScript XML)
+export default function Header() {
+  return (
+    <header>
+      <h1>Be The Hero</h1>
+    </header>
+  );
+}
