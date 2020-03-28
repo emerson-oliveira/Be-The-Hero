d072171283ce49b432f5ab049e48761064b3351c
Emerson Oliveira <emerson-ods@hotmail.com>
2020-03-27 22:47:15 -0300

    unit test update

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,17 +1,15 @@
 /* My Project Semana OmniStack */
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes');
 
-const app = express();
-const port = 3333;
 
-/**produção */
-// app.use(cors({
-//     origin: 'http://meuapp.com'
-// }));
+const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+module.exports = app; 
